5b4650a5884e746556eaf80f6213dbc6530c92f3
Lucas Merold <Lucas.merold@yahoo.de>
2020-10-06 10:29:48 +0200

    basic ready

---
+++ basic.js
@@ -0,0 +1,17 @@
+
+$('.bell').on('click', function(){
+    $('#result').html('You clicked on the Christmas bell')
+})
+
+
+// $('.santa').on('click', function(){
+//     $('#result').html('You clicked on the Christmas bell')
+// })
+
+$('.santa').on('click', function(){
+    $(this).css('display','none')
+})
+
+
+
+// $('div').css('background-color', 'yellow')
